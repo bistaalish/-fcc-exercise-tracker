0ec194449efa3304815d33bdbc120947e2018145
Alish Bista <bistace321@gmail.com>
2023-08-22 04:18:02 +0545

    feat: usermodel completed

--- model/user.js
+++ model/user.js
@@ -1,10 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true,"Must include a username"]
-  }  
-})
+    username: {
+      type:String,
+      required: [true,"Must include a username"]
+    },
+});
 
-module.exports = mongoose.module('User',userSchema);
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
