d0208663ed3d1d488e6238220aa1946c4631c25f
Alish Bista <bistace321@gmail.com>
2023-08-22 04:48:10 +0545

    feat: exercise model over

--- model/exercise.js
+++ model/exercise.js
@@ -1,21 +1,29 @@
+/**
+ * ! This is a exercise Model
+ * @param userId description, date 
+ */
 const mongoose = require('mongoose');
+
 const exerciseSchema = new mongoose.Schema({
     userId: {
         type: String,
-        required: [true, "Must include a userId"]
+        required: [true,"Must include a userId"]
+    },
+    duration: {
+        type: String,
+        required: [true, "Must include a duration"]
     },
     description: {
         type: String,
-        required: [true, "Must include a description"]
-    },
-    duration: {
-        type: Number,
-        required: [true, "Must include a duration"]
+        required: [true,"Must include a description"]
     },
     date: {
         type: String,
-        default: new Date().toDateString()
+        default:  new Date().toDateString()
     }
-})
+    
+});
 
-module.exports = mongoose.model('Exercise', exerciseSchema);
+const Exercise = mongoose.model('exercise', exerciseSchema)
+
+module.exports = Exercise
